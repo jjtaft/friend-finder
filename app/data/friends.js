f663b42369fd9d90b26f8c531b9d50be18c48e67
Justin Taft <jjtaft@gmail.com>
2017-05-06 09:18:45 -0500

    Add files via upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,33 @@
-friends.js
+[
+  {
+    "name": "Fanny Pack Guy",
+    "photo": "http://funhuh.com/wp-content/uploads/2014/06/crazy-people.jpg",
+    "scores": [
+      "2",
+      "1",
+      "5",
+      "5",
+      "5",
+      "3",
+      "3",
+      "2",
+      "4",
+      "4"
+    ]
+  },
+  {
+    "name": "Miss Juggaloo",
+    "photo": "http://www.funnypica.com/wp-content/uploads/2013/02/Crazy-Funny-People-34-FunnyPica.com_.jpg",
+    "scores": [
+      "4",
+      "3",
+      "1",
+      "1",
+      "5",
+      "4",
+      "3",
+      "1",
+      "5",
+      "4"
+    ]
+  },
